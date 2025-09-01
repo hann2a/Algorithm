ccb973c992d218cd594116d8836fddf6dc3da161
hann2a <hkanhanea@gmail.com>
2025-09-01 18:01:13 +0900

    250901

--- 0829_DFS/01_graph/01_make_adj_matrix.py
+++ 0829_DFS/01_graph/01_make_adj_matrix.py
@@ -12,6 +12,11 @@ edge_data =  list(map(int, input().split()))
 
 # 1. V + 1 * V + 1 크기의 2차원 리스트를 0으로 초기화 
 adj_matrix = [[0] * (V+1) for _ in range(V+1)]
-print(adj_matrix)
+# print(adj_matrix)
 
+# 2. 간선 정보를 바탕으로 2개씩 짝지어서 인접 행렬에 표기 
+# 총 표기 회수는 간선의 개수 만큼(E번)
+for i in range(E):
+    n1, n2 = edge_data[i*2], edge_data[i * 2 +1]
+    adj_matrix[n1]
 # --- 결과 확인 ---
---
+++ 0901_BFS/01_bfs_adj_matrix.py
@@ -0,0 +1,49 @@
+import sys
+from collections import deque
+
+sys.stdin = open('input.txt')
+
+
+def bfs_matrix(start_node, V, adj_matrix):
+    """
+    큐(deque)와 인접 행렬을 사용한 BFS
+    """
+    visited = [False] * (V + 1)  # 각 노드의 방문 여부를 기록
+    path = []  # 최종 탐색 경로를 저장
+    # BFS는 큐를 사용. 파이썬 list의 pop(0)은 비효율적이므로 deque 사용
+    q = deque()
+
+    # --- BFS 시작 처리 ---
+    # 1. 시작 노드를 방문 처리를 하고 큐에 삽입 
+    # 큐에 넣었다는 것은 이 노드를 발견해서 처리할 거라는 의미 
+    q.append(start_node)
+    # 큐가 비어있지 않은 동안 반복(큐가 빌 때까지 탐색 진행)
+    while q:
+        # 2. 큐에서 노드를 하나 꺼냄(deque)
+        current_node = q.popleft()
+        path.append(current_node)
+
+        # 3. 현재 노드와 인접한 모든 인접 노드를 확인
+        # 인접 행렬은 모든 노드 (1~V)를 순회하며 연결 여부를 확인해야 함 
+        for next_node in range(1, V+1):
+            # 조건1: next_node가 current_node와 인접해있는가?
+            # 조건2: next_node가 아직 방문한 적이 없는가?
+            if adj_matrix[current_node][next_node] == 1 and not visited[next_node]:
+                # 4. 방문 처리 후 큐에 삽입 (enqueue)
+                # 두 조건을 모두 만족하면, 다음 방문 대상으로 확정
+                visited[next_node] = True
+                q.append(next_node)
+
+# --- 그래프 구성 ---
+V, E = map(int, input().split())
+data = list(map(int, input().split()))
+
+adj_matrix = [[0] * (V + 1) for _ in range(V + 1)]
+for i in range(E):
+    n1, n2 = data[i * 2], data[i * 2 + 1]
+    adj_matrix[n1][n2] = 1
+    adj_matrix[n2][n1] = 1
+
+# --- BFS 실행 ---
+result_path = bfs_matrix(1, V, adj_matrix)
+print(''.join(map(str, result_path)))
